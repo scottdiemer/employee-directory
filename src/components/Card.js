83a578f90a78e2794e48915c16bd336c6550aa75
scottdiemer <sdiemer@cfl.rr.com>
2019-01-20 20:48:16 -0500

    adjusted card shadow

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({ name, email, phone, picture }) => {
   return (
-    <div className='tc bg-washed-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+    <div className='tc bg-washed-blue dib br3 pa3 ma2 grow bw2 shadow-2'>
       <img src={picture} alt='employees' />
       <div>
         <h2>{name}</h2>
